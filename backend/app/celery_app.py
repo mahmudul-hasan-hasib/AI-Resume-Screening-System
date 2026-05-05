92ecef0209e95362e678cfb800cd5b0630b54cc7
Mahmudul Hasan Hasib <125702889+mahmudul-hasan-hasib@users.noreply.github.com>
2026-05-06 04:27:33 +0600

    fix: use env redis url

--- backend/app/celery_app.py
+++ backend/app/celery_app.py
@@ -1,10 +1,12 @@
+import os
 from celery import Celery
+
+REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
 
 celery = Celery(
     "worker",
-    broker="redis://redis:6379/0",
-    backend="redis://redis:6379/0"
+    broker=REDIS_URL,
+    backend=REDIS_URL
 )
-
 
 celery.autodiscover_tasks(["app"])
